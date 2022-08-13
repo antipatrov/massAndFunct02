600fde8cb7216765082d139332048a737ebf6969
Yuri_Antipatrov <89209578005@yandex.ru>
2022-08-13 16:08:58 +0300

    Task019

---
+++ Task019/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 19
+
+//Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(number);
+
+if (stringNumber[0] == stringNumber[4] && stringNumber[1] == stringNumber[3])
+{
+  Console.WriteLine(" YES, tnis is number-palindrome") ;
+}
+else {
+  Console.WriteLine("NO, tnis is number-not a palindrome ");
+}
